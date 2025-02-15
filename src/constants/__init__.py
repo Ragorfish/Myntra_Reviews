f211161d93346ba3ccf1d9e1ccd736e61f3eca54
MohitSisodia-fullstack <mohitsisodia.2000.10@gmail.com>
2025-02-15 21:09:06 +0530

    first commit

---
+++ src/constants/__init__.py
@@ -0,0 +1,5 @@
+MONGODB_URL_KEY: str = "mongodb+srv://mohitsisodia200010:dBKGAd3VHezEAZhD@cluster0.lii1w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DATABASE_NAME: str = "myntra-reviews"
+
+
+SESSION_PRODUCT_KEY: str = "product_name"
